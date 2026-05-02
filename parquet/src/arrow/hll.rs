3cd73594b02609cd204b1b9855ff7372505d0435
Jan Kaul <jankaul@mailbox.org>
2026-05-02 10:27:54 -0700

    use parallel computation

--- parquet/src/arrow/hll.rs
+++ parquet/src/arrow/hll.rs
@@ -1,7 +1,7 @@
 use arrow_array::{
-    ArrayRef, PrimitiveArray,
+    ArrayRef, PrimitiveArray, UInt8Array,
     cast::as_primitive_array,
-    types::{Int64Type, UInt16Type, UInt64Type},
+    types::{Int64Type, UInt8Type, UInt64Type},
 };
 use twox_hash::XxHash64;
 
@@ -18,16 +18,19 @@ impl HyperLogLog {
         let array: &PrimitiveArray<Int64Type> = as_primitive_array(&array);
         let hashes: PrimitiveArray<UInt64Type> =
             array.unary(|v| XxHash64::oneshot(HLL_HASH_SEED, &v.to_le_bytes()));
-        let packed: PrimitiveArray<UInt16Type> = hashes.unary(|h| {
-            let index = (h >> 56) as u16;
-            let lz = (h << 8).leading_zeros() as u16;
-            (index << 8) | lz
-        });
-        for entry in packed.iter().flatten() {
-            let [index, lz] = entry.to_be_bytes();
-            let register = &mut self.registers[index as usize];
-            if lz > *register {
-                *register = lz;
+        let indices: UInt8Array = hashes.unary(|h| (h >> 56) as u8);
+        let leading_zeros: UInt8Array = hashes.unary(|h| (h << 8).leading_zeros() as u8);
+
+        for i in 0..=255u8 {
+            let scalar = UInt8Array::new_scalar(i);
+            let mask = arrow_ord::cmp::eq(&indices, &scalar)?;
+            let filtered = arrow_select::filter::filter(&leading_zeros, &mask)?;
+            let filtered: &PrimitiveArray<UInt8Type> = as_primitive_array(&filtered);
+            if let Some(max_lz) = arrow_arith::aggregate::max(filtered) {
+                let register = &mut self.registers[i as usize];
+                if max_lz > *register {
+                    *register = max_lz;
+                }
             }
         }
         Ok(())
