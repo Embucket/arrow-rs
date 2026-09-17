c992af1276ac5aabac2df59b89d6993525b8dbfc
osipovartem <artemosipov.dev@gmail.com>
2026-09-18 01:05:31 +0300

    Add recoverable CSV record error handler

--- arrow-csv/src/lib.rs
+++ arrow-csv/src/lib.rs
@@ -29,9 +29,9 @@
 pub mod reader;
 pub mod writer;
 
-pub use self::reader::Reader;
-pub use self::reader::ReaderBuilder;
-pub use self::reader::infer_schema_from_files;
+pub use self::reader::{
+    CsvRecordError, CsvRecordErrorHandler, Reader, ReaderBuilder, infer_schema_from_files,
+};
 pub use self::writer::QuoteStyle;
 pub use self::writer::Writer;
 pub use self::writer::WriterBuilder;
